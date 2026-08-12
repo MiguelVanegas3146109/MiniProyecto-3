5de8445db1337ddc3cf3016058e427c4ffa9f7fd
MiguelVanegas3146109 <vanegasmiguelangel89@gmail.com>
2026-08-12 11:00:40 -0500

    Nuevo sweet

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('friendButton').addEventListener('click', function() {
+    Swal.fire({
+        title: 'Amistad',
+        text: 'La Amistad es un tesoro invaluable ¡Gracias por ser mi amigo!',
+        icon: 'success',
+        confirmButtonText: 'Cerrar'
+    });
+});
